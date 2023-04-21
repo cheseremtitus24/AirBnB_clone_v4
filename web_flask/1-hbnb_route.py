eb01e47ed6343d296b2a53c6edac3d3b7d6ddf30
Titus Kemboi Cheserem <cheseremtitus24@gmail.com>
2023-04-21 19:49:14 +0300

    Fix,Debug: Added python3 intepreter directive and used os module to set env_var

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,32 +1,31 @@
+#!/usr/bin/python3
 """
 This module Utilizes basic routes and simple
 html display
 """
 from flask import Flask
+import os
+os.environ["FLASK_APP"] = "1-hbnb_route.py"
 
 # Function that creates the app
 
 
-def create_app(test_config=None):
-    """ This is the main function that is called
-    when creating a flask app and must be returned
-    """
-    app = Flask(__name__)
+app = Flask(__name__)
 
-    @app.route('/', strict_slashes=False)
-    def hello_school():
-        """ Function returns a very basic html string without any tags"""
-        return 'Hello HBNB!'
 
-    @app.route('/hbnb', strict_slashes=False)
-    def school():
-        """ Function returns a very basic html string without any tags"""
-        return 'HBNB'
+@app.route('/', strict_slashes=False)
+def hello_school():
+    """ Function returns a very basic html string without any tags"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def school():
+    """ Function returns a very basic html string without any tags"""
+    return 'HBNB'
 
     return app
 
 
-# Create the App
-APP = create_app()
 if __name__ == '__main__':
-    APP.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
