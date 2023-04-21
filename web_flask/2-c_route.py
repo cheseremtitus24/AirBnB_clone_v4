40091835e1651ce41b553aea5990071861f3fd88
Titus Kemboi Cheserem <cheseremtitus24@gmail.com>
2023-04-21 19:55:17 +0300

    Fix,Debug: Added python3 intepreter directive and used os module to set env_var

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,41 +1,37 @@
+#!/usr/bin/python3
 """
 This module Utilizes basic routes and simple
 html display
 """
 from flask import Flask, escape
+import os
+os.environ["FLASK_APP"] = "2-c_route.py"
 
 # Function that creates the app
+app = Flask(__name__)
 
 
-def create_app(test_config=None):
-    """ This is the main function that is called
-    when creating a flask app and must be returned
-    """
-    app = Flask(__name__)
+@app.route('/', strict_slashes=False)
+def hello_school():
+    """ Function returns a very basic html string without any tags"""
+    return 'Hello HBNB!'
 
-    @app.route('/', strict_slashes=False)
-    def hello_school():
-        """ Function returns a very basic html string without any tags"""
-        return 'Hello HBNB!'
 
-    @app.route('/hbnb', strict_slashes=False)
-    def school():
-        """ Function returns a very basic html string without any tags"""
-        return 'HBNB'
+@app.route('/hbnb', strict_slashes=False)
+def school():
+    """ Function returns a very basic html string without any tags"""
+    return 'HBNB'
 
-    @app.route('/c/<text>', strict_slashes=False)
-    def c_func(text):
-        """ Function returns a very basic html string and displays parameter
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_func(text):
+    """ Function returns a very basic html string and displays parameter
         values and substitutes underscores with an empty space.
         """
-        text = text.split('_')
-        text = " ".join(text)
-        return 'C %s' % escape(text)
-
-    return app
+    text = text.split('_')
+    text = " ".join(text)
+    return 'C %s' % escape(text)
 
 
-# Create the App
-APP = create_app()
 if __name__ == '__main__':
-    APP.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
