a33a26859514293e5f7842648d93e7514839f4e5
Titus Kemboi Cheserem <cheseremtitus24@gmail.com>
2023-04-21 19:42:32 +0300

    modified:   0-hello_route.py

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -4,24 +4,21 @@ This module Utilizes basic routes and simple
 html display
 """
 from flask import Flask
+import os
+os.environ["FLASK_APP"] = "0-hello_route.py"
 
 # Function that creates the app
 
 
-def create_app(None):
-    """ This is the main function that is called
-    when creating a flask app and must be returned
-    """
-    app = Flask(__name__)
+app = Flask(__name__)
 
-    @app.route('/', strict_slashes=False)
-    def hello_world():
-        """ Function returns a very basic html string without any tags"""
-        return 'Hello HBNB!'
-    return app
+
+@app.route('/', strict_slashes=False)
+def hello_world():
+    """ Function returns a very basic html string without any tags"""
+    return 'Hello HBNB!'
 
 
 # Create the App
-APP = create_app()
 if __name__ == '__main__':
-    APP.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
