82ea6295bed54dbc1f52ce0b08996611644443cb
Titus Kemboi Cheserem <cheseremtitus24@gmail.com>
2023-04-21 20:02:42 +0300

    Fix,Debug: Added python3 intepreter directive and used os module to set env_var

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -1,59 +1,61 @@
+#!/usr/bin/python3
 """
 This module Utilizes basic routes and simple
 html display
 """
 from flask import Flask, escape
+import os
+os.environ["FLASK_APP"] = "2-c_route.py"
 
 # Function that creates the app
 
 
-def create_app(test_config=None):
-    """ This is the main function that is called
-    when creating a flask app and must be returned
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_school():
+    """ Function returns a very basic html string without any tags"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def school():
+    """ Function returns a very basic html string without any tags"""
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_func(text):
+    """ Function returns a very basic html string and displays parameter
+    values and substitutes underscores with an empty space.
     """
-    app = Flask(__name__)
-
-    @app.route('/', strict_slashes=False)
-    def hello_school():
-        """ Function returns a very basic html string without any tags"""
-        return 'Hello HBNB!'
-
-    @app.route('/hbnb', strict_slashes=False)
-    def school():
-        """ Function returns a very basic html string without any tags"""
-        return 'HBNB'
-
-    @app.route('/c/<text>', strict_slashes=False)
-    def c_func(text):
-        """ Function returns a very basic html string and displays parameter
-        values and substitutes underscores with an empty space.
-        """
-        text = text.split('_')
-        text = " ".join(text)
-        return 'C %s' % escape(text)
-
-    @app.route('/python', strict_slashes=False)
-    @app.route('/python/<text>', strict_slashes=False)
-    def python_func(text="is cool"):
-        """ Function returns a very basic html string and displays parameter
-        values and substitutes underscores with an empty space. Also supports default parameter arguments
-        """
-        text = text.split('_')
-        text = " ".join(text)
-        return 'Python %s' % escape(text)
-
-    @app.route('/number/<int:num>', strict_slashes=False)
-    def int_display(num):
-        """ param num:
-                        must be an integer value.
-            description:
-                        Renders Integer Values only
-        """
-        return '%d is a number' % num
-    return app
+    text = text.split('_')
+    text = " ".join(text)
+    return 'C %s' % escape(text)
 
 
-# Create the App
-APP = create_app()
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_func(text="is cool"):
+    """ Function returns a very basic html string and displays parameter
+    values and substitutes underscores with an empty space.
+    Also supports default parameter arguments
+    """
+    text = text.split('_')
+    text = " ".join(text)
+    return 'Python %s' % escape(text)
+
+
+@app.route('/number/<int:num>', strict_slashes=False)
+def int_display(num):
+    """ param num:
+                    must be an integer value.
+        description:
+                    Renders Integer Values only
+    """
+    return '%d is a number' % num
+
+
 if __name__ == '__main__':
-    APP.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
