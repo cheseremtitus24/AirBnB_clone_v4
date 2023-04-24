c44718878e487a1dcbe810085c11d0aa7536ffdf
Titus Kemboi Cheserem <cheseremtitus24@gmail.com>
2023-04-24 08:58:46 +0300

    Update: Added a teardown on the established db connection after every request Cleaned up unused Function definitions

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -14,73 +14,6 @@ os.environ["FLASK_APP"] = "7-states_list.py"
 app = Flask(__name__)
 
 
-@app.route('/', strict_slashes=False)
-def hello_school():
-    """ Function returns a very basic html string without any tags"""
-    return 'Hello HBNB!'
-
-
-@app.route('/hbnb', strict_slashes=False)
-def school():
-    """ Function returns a very basic html string without any tags"""
-    return 'HBNB'
-
-
-@app.route('/c/<text>', strict_slashes=False)
-def c_func(text):
-    """ Function returns a very basic html string and displays parameter
-    values and substitutes underscores with an empty space.
-    """
-    text = text.split('_')
-    text = " ".join(text)
-    return 'C %s' % escape(text)
-
-
-@app.route('/python', strict_slashes=False)
-@app.route('/python/<text>', strict_slashes=False)
-def python_func(text="is cool"):
-    """ Function returns a very basic html string and displays parameter
-    values and substitutes underscores with an empty space.
-    Also supports default parameter arguments
-    """
-    text = text.split('_')
-    text = " ".join(text)
-    return 'Python %s' % escape(text)
-
-
-@app.route('/number/<int:num>', strict_slashes=False)
-def int_display(num):
-    """ param num:
-                    must be an integer value.
-        description:
-                    Renders Integer Values only
-    """
-    return '%d is a number' % num
-
-
-@app.route('/number_template/<int:num>', strict_slashes=False)
-def int_display_template(num):
-    """ param num:
-                    must be an integer value.
-        description:
-                    Renders Integer Values on a
-                    template file
-    """
-    return render_template('5-number.html', number=num)
-
-
-@app.route('/number_odd_or_even/<int:num>', strict_slashes=False)
-def int_even_odd_display_template(num):
-    """ param num:
-                    must be an integer value.
-        description:
-                    Renders Integer Values and
-                    conditionally displays content
-                    of the template file
-    """
-    return render_template('6-number_odd_or_even.html', number=num)
-
-
 @app.route('/states_list', strict_slashes=False)
 def state_list():
     """ Display Listing of all States:
@@ -89,8 +22,13 @@ def state_list():
 
     return render_template('7-states_list.html', states=states)
 
+
 @app.teardown_appcontext
 def shutdown_session(exception=None):
+    """
+    tears down a databases connection after every
+    completed get request
+    """
     storage.close()
 
 
