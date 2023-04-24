49913af765cc15e9e7de066cf29fa40ac606a2fb
Titus Kemboi Cheserem <cheseremtitus24@gmail.com>
2023-04-24 08:55:52 +0300

    Update: Added a teardown on the established db connection after every request

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -89,6 +89,10 @@ def state_list():
 
     return render_template('7-states_list.html', states=states)
 
+@app.teardown_appcontext
+def shutdown_session(exception=None):
+    storage.close()
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
