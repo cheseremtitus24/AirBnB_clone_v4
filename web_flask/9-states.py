e86a61b3e91c6cc1a1336bdbbb34210dcd144734
Titus Kemboi Cheserem <cheseremtitus24@gmail.com>
2023-04-25 17:32:25 +0300

    Feature: Support Display Content using Cross File Access + supports DB + File Storage Parsing & display of Content

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -8,6 +8,8 @@ files
 from flask import Flask, escape, render_template
 import os
 from models import storage
+from models.state import State
+from models.city import City
 STORAGE_TYPE = os.environ.get('HBNB_TYPE_STORAGE')
 os.environ["FLASK_APP"] = "7-states_list.py"
 
@@ -18,8 +20,10 @@ app = Flask(__name__)
 def state_list():
     """ Display Listing of all States:
     """
-    states = storage.all('State')
-
+    if STORAGE_TYPE == "db":
+        states = storage.all('State')
+    else:
+        states = storage.all(State)
     return render_template('7-states_list.html', states=states)
 
 
@@ -29,10 +33,11 @@ def cities_by_states():
     when storage is db set flag to true else set to false
     When storage is other use the public getter method cities
     """
-    states = storage.all('State')
     if STORAGE_TYPE == "db":
+        states = storage.all('State')
         flag = True
     else:
+        states = storage.all(City).values()
         flag = False
     # cities = storage.state_cities("421a55f4-7d82-47d9-b54c-a76916479545")
     return render_template(
@@ -54,23 +59,34 @@ def get_states(text="all"):
     the page displays "Not found!"
     """
     if text == "all":
-        states = storage.all('State')
-
+        if STORAGE_TYPE == "db":
+            states = storage.all('State')
+        else:
+            states = storage.all(State)
         return render_template('7-states_list.html', states=states)
-    else:
-        states = storage.all('State')
-        # cities = storage.state_cities("421a55f4-7d82-47d9-b54c-a76916479545")
-        dummy = list()
-        for state in states.values():
-            if state.id == text:
-                states = state
-                dummy.append(states)
 
-        return render_template(
-            '9-states.html',
-            states=dummy,
-            storage=storage,
-            condition=len(dummy))
+    else:
+        if STORAGE_TYPE == "db":
+            states = storage.all('State')
+        # cities = storage.state_cities("421a55f4-7d82-47d9-b54c-a76916479545")
+            dummy = list()
+            for state in states.values():
+                if state.id == text:
+                    states = state
+                    dummy.append(states)
+
+            return render_template(
+                '9-states.html',
+                states=dummy,
+                storage=storage,
+                condition=len(dummy))
+        else:
+            states = storage.all(City).values()
+            return render_template(
+                '8-cities_by_states.html',
+                states=states,
+                storage=storage,
+                city_decision=0)
 
 
 @app.teardown_appcontext
